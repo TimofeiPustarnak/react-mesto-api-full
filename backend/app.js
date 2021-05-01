00e9f1c4d6e6dafb698bec2ea99a761d61c347fa
Timofei Pustarnak <pustimafei@gmail.com>
2021-05-01 08:45:24 +0300

    C:/Program Files/Git/

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const { errors } = require("celebrate");
-const cookieParser = require("cookie-parser");
-const cors = require("./middlewares/cors.js");
-const helmet = require("helmet");
-const users = require("./routers/users");
-const cards = require("./routers/cards");
-const { createUser, login } = require("./controllers/users");
-const { validateNewUser, validateLogin } = require("./middlewares/validation");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const cors = require('./middlewares/cors.js');
+const users = require('./routers/users');
+const cards = require('./routers/cards');
+const { createUser, login } = require('./controllers/users');
+const { validateNewUser, validateLogin } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const auth = require("./middlewares/auth");
+const auth = require('./middlewares/auth');
 
 const corsOptions = {
   origin: [
-    "https://Timofei.Pustarnak.nomoredomains.icu",
-    "http://Timofei.Pustarnak.nomoredomains.icu",
+    'https://Timofei.Pustarnak.nomoredomains.icu',
+    'http://Timofei.Pustarnak.nomoredomains.icu',
   ],
   credentials: true,
 };
@@ -30,30 +30,30 @@ app.use(requestLogger);
 app.use(cookieParser());
 app.use(helmet());
 
-app.post("/signin", validateLogin, login);
-app.post("/signup", validateNewUser, createUser);
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateNewUser, createUser);
 
 app.use(auth);
-app.use("/", cards);
-app.use("/", users);
+app.use('/', cards);
+app.use('/', users);
 app.use(errorLogger);
 app.use(errors());
 
 app.use((req, res) => {
-  res.status(404).send({ error: 404, message: "ресурс не найден" });
+  res.status(404).send({ error: 404, message: 'ресурс не найден' });
 });
 
 app.use((error, req, res, next) => {
   const { statusCode = 500, message } = error;
 
   res.status(statusCode).send({
-    message: statusCode === 500 ? "На сервере произошла ошибка." : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка.' : message,
   });
 
   next();
 });
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
