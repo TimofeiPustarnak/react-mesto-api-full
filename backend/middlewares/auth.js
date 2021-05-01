7f092747ed387241cee906a44074960e82ea1a55
Timofei Pustarnak <pustimafei@gmail.com>
2021-05-01 10:01:42 +0300

    C:/Program Files/Git/

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,11 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const authorization = req.cookies.jwt;
 
   if (!authorization) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    console.log(1);
+    return res.status(401).send({ message: "Необходима авторизация" });
   }
 
   const token = authorization;
@@ -13,10 +14,11 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(
       token,
-      '2061f1dbc12f53401a57d915f2e090cbca576b875e5e774d29cfc2462ce2d27d',
+      "2061f1dbc12f53401a57d915f2e090cbca576b875e5e774d29cfc2462ce2d27d"
     );
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    condole.log(100);
+    return res.status(401).send({ message: "Необходима авторизация" });
   }
 
   req.user = payload;
