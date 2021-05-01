49e237060d0b9f25ae72e662420caf3195f1a199
Timofei Pustarnak <pustimafei@gmail.com>
2021-05-01 08:36:35 +0300

    C:/Program Files/Git/

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,9 +1,9 @@
-const cors = require("cors");
+const cors = require('cors');
 
 const whitelist = [
-  "http://localhost:3001",
-  "http://timofei.pustarnak.nomoredomains.icu",
-  "https://timofei.pustarnak.nomoredomains.icu",
+  'http://localhost:3001',
+  'http://timofei.pustarnak.nomoredomains.icu',
+  'https://timofei.pustarnak.nomoredomains.icu',
 ];
 
 const corsOptions = {
@@ -11,18 +11,18 @@ const corsOptions = {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization',
   ],
-  exposedHeaders: ["Set-Cookie"],
+  exposedHeaders: ['Set-Cookie'],
   credentials: true,
 };
 
